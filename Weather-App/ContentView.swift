df280d41dfc41a7c1c51f47c904f6c68f75dc505
Vicgri <89450050+Vicgri@users.noreply.github.com>
2023-09-22 16:08:36 +0200

    background color and text

--- Weather-App/ContentView.swift
+++ Weather-App/ContentView.swift
@@ -8,17 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    ZStack
+    {
+      LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]),
+                     startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/,
+                     endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+      
+      
+      VStack 
+      {
+        Text("Bergen, Norway")
+          .font(.system(size: 32, weight: .medium, design: .default))
+          .foregroundColor(.white)
+          .padding()
+        Spacer()
+        
+      }
     }
+    
+    
+  }
 }
 
 #Preview {
-    ContentView()
-}
+  ContentView()
+};
