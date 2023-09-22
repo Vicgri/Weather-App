4df0d93bf76364527a9f3fbc62f4439a51c05238
Vicgri <89450050+Vicgri@users.noreply.github.com>
2023-09-22 16:44:15 +0200

    Added weather icons and text

--- Weather-App/ContentView.swift
+++ Weather-App/ContentView.swift
@@ -14,23 +14,131 @@ struct ContentView: View {
       LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]),
                      startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/,
                      endPoint: .bottomTrailing)
-        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+      .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
       
-      
-      VStack 
+      VStack
       {
         Text("Bergen, Norway")
           .font(.system(size: 32, weight: .medium, design: .default))
           .foregroundColor(.white)
           .padding()
-        Spacer()
         
+        VStack(spacing: 8)
+        {
+          Image(systemName: "cloud.sun.fill")
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 180, height: 180)
+          
+          Text("15°C")
+            .font(.system(size: 70, weight: .medium))
+            .foregroundColor(.white)
+          
+        }
+        HStack (spacing: 10) {
+      
+          VStack
+          {
+            Text("MAN")
+              .font(.system(size: 20))
+              .bold()
+              .foregroundColor(.white)
+            
+            Image(systemName: "cloud.sun.fill")
+              .renderingMode(.original)
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 50, height: 50)
+              .padding(3)
+            
+            Text("18°")
+              .foregroundColor(.white)
+              .font(.system(size: 22, weight: .bold))
+          }
+          VStack
+          {
+            Text("TIR")
+              .font(.system(size: 20))
+              .bold()
+              .foregroundColor(.white)
+            
+            Image(systemName: "cloud.fill")
+              .renderingMode(.original)
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 50, height: 50)
+              .padding(3)
+            
+            Text("13°")
+              .foregroundColor(.white)
+              .font(.system(size: 22, weight: .bold))
+          }
+          VStack
+          {
+            Text("ONS")
+              .font(.system(size: 20))
+              .bold()
+              .foregroundColor(.white)
+            
+            Image(systemName: "cloud.sun.rain.fill")
+              .renderingMode(.original)
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 50, height: 50)
+              .padding(3)
+            
+            Text("16°")
+              .foregroundColor(.white)
+              .font(.system(size: 22, weight: .bold))
+          }
+          VStack
+          {
+            Text("TORS")
+              .font(.system(size: 20))
+              .bold()
+              .foregroundColor(.white)
+            
+            Image(systemName: "sun.max.fill")
+              .renderingMode(.original)
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 50, height: 50)
+              .padding(3)
+            
+            Text("22°")
+              .foregroundColor(.white)
+              .font(.system(size: 22, weight: .bold))
+          }
+          VStack
+          {
+            Text("FRE")
+              .font(.system(size: 20))
+              .bold()
+              .foregroundColor(.white)
+            
+            Image(systemName: "wind")
+              .renderingMode(.original)
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 50, height: 50)
+              .padding(3)
+            
+            Text("20°")
+              .foregroundColor(.white)
+              .font(.system(size: 22, weight: .bold))
+          }
+          
+        }
+        .padding()
       }
+      Spacer()
     }
-    
-    
   }
+  
+  
 }
+
 
 #Preview {
   ContentView()
