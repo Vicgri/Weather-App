090667dc8482a319bbae8a5c6202336463e4361a
Vicgri <89450050+Vicgri@users.noreply.github.com>
2023-09-22 15:22:13 +0200

    Initial Commit

---
+++ Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by Victoria Grimen on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
